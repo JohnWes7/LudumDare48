a91540af75244d21b4b050cba57345fd84e1aab7
JohnWes7 <1246215651@qq.com>
2021-04-24 16:31:48 +0800

    animation

---
+++ Ludum Dare/Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody2D mRigibody;
+    private float Horizontal;
+    private float Vertical;
+
+    void Update()
+    {
+        Horizontal = Input.GetAxisRaw("Horizontal");
+        Vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
