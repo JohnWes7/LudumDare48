890897e9e1195137324831133837bf82ddcc8b34
ntryyds <973949858@qq.com>
2021-04-24 17:15:20 +0800

    recall

---
+++ Ludum Dare/Assets/Scripts/Controller/StartPannelController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartPannelController : MonoBehaviour
+{
+    public void StartPannelCallback()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
